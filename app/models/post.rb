e2e62ff7624888f62a1f35739711e9f38818b936
Pedro Souza <pedrosport10@gmail.com>
2023-10-02 13:54:24 -0300

    Reduzindo tamanho do nome e melhroando o show

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_many :comentarios, dependent: :destroy
-  validates :title, presence: true, length: {minimum: 5}
-  validates :content, presence: true, length: {minimum: 10}
+  validates :title, presence: true, length: {minimum: 3}
+  validates :content, presence: true, length: {minimum: 5}
 end
