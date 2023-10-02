c97f3a29c32c9c290e2645a7c124e09df058ac3f
Pedro Souza <pedrosport10@gmail.com>
2023-10-02 00:55:21 -0300

    Melhorando os formularios e o index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_many :comentarios, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
-  validates :content, presence: true, length: {minimum: 10, maximum:50}
+  validates :content, presence: true, length: {minimum: 10}
 end
